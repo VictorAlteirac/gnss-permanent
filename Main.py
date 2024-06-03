ea69b01bfbe393daf27cb66cd82b054020a5fe62
Totor <79082220+VictorAlteirac@users.noreply.github.com>
2024-06-03 18:17:39 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -8,30 +8,285 @@ Created on Mon May 27 10:29:08 2024
 from utils import *
 import gnsscal
 from datetime import date
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from matplotlib import dates
+import tkinter as tk
 
-year=2024
-month=5
-day=18
+
+
 
 #Definition des variables par défaut
-gnss_day=gnsscal.date2doy(date(year, month, day))
-gnss_week=gnsscal.date2gpswd(date(year, month, day))
+def dobnwload_gnss(year, month, day): 
+    gnss_day=gnsscal.date2doy(date(year, month, day))
+    gnss_week=gnsscal.date2gpswd(date(year, month, day))
+    
+    print(gnss_day)
+    
+    py_path=os.getcwd()
+    
+    station_list_sira=['mtp2', 'mntp', 'yscn', 'gajn', 'sgil']
+    station_list_stein=['pzna', 'agds', 'agde', 'narb', 'pard', 'mtp2']
+    site='SIRA'
+    
+    if site == 'SIRA': 
+        site_num=str('0712')
+    if site == 'STEIN': 
+        site_num=str('0830')
+        
+    path_download=py_path+"\DOWNLOAD\Donwload_file_RGP"
+    
+    #Récuparation des données d'observation et de navigation
+    file_path=path_download+'/'+site+'/'+str(year)+'/'+str(gnss_day)+'/obs'
+    file_path_nav=path_download+'/'+site+'/'+str(year)+'/'+str(gnss_day)+'/nav'
+    text_box.delete('1.0', tk.END)
+    
+    try: 
+        print('Telechargement des fichiers RINEX d\'observation...')
+        download_rinex_obs(year, gnss_day, station_list_sira, site, path_download)
+        uncrompress_Z_file(file_path)
+        uncompact_rinex_file_hatanaka(file_path)
+        delete_useless_file(file_path)
+    
+        print('Telechargement des fichiers RINEX de navigation...')
+        uncrompress_Z_file_nav(file_path_nav)
+    except: 
+        text_box.insert("end", "fichier Rinex RGP manquant ou deja telechargé \n") # adds text to text bo
+    
+    
+    print('Telechargement des fichiers RINEX de l\'antenne...')
+    try: 
+        Donwload_SIRA_day(year, month, day, station_list_sira, site, path_download, gnss_day)
+        unzip_SIRA_file(year, gnss_day)
+    except: 
+        text_box.insert("end", "fichier Rinex SIRA manquant ou deja telechargé \n") # adds text to text bo
+    
+    try: 
+        print('Telechargement des éphémérides precise et correction horloge...')
+        Donwload_IGS_product(gnss_week, gnss_day, year)
+    except: 
+        text_box.insert("end", "Ephémérides ou clock manquant ou indisponible \n")
+        
+    text_box.insert("end", "Données télécharger avec succees \n")  
+        
+    print('Telechargement terminée')
+    
 
-py_path=os.getcwd()
+def process_gnss(year, month, day):
+    
+    py_path=os.getcwd()
+    
+    text_box.delete('1.0', tk.END)
+    
+    station_list_sira=['mtp2', 'mntp', 'yscn', 'gajn', 'sgil']
+    station_list_stein=['pzna', 'agds', 'agde', 'narb', 'pard', 'mtp2']
+    site='SIRA'
+    site_num='0712'
+    
+    gnss_day=gnsscal.date2doy(date(year, month, day))
+    gnss_week=gnsscal.date2gpswd(date(year, month, day))
+    
+    print("CALCUL DES COORDONNEES SUR PLUSIEURS STATIONS...")
+    coordinates_stat=[]
+    observation_time=[]
+    base_line=[]
+    for rgp in tqdm(station_list_sira):
+        
+        try: 
+            out_file=calcul_GNSS_RTK_LIB(site, site_num ,year, month, day, gnss_day, gnss_week, rgp)
+            file = open(out_file, "r")
+            result = file.readlines()
+            ref_coordinates=result[23]
+            
+            time=int(result[7][28:30])
+            observation_time.append(time)
+            
+            result=result[-1]
+            array=np.array([float(result[26:38]), float(result[41:52]), float(result[56:68])])
+            coordinates_stat.append(array)
+            
+            array_ref=np.array([float(ref_coordinates[15:26]), float(ref_coordinates[29:40]), float(ref_coordinates[42:54])])
+            
+            baseline=math.sqrt(abs(array[0]**2-array_ref[0]**2)+abs(array[1]**2-array_ref[1]**2)+abs(array[2]**2-array_ref[2]**2))
+            print(baseline)
+            base_line.append(baseline)
+        except: 
+            text_box.insert("end", "Eurreur sur l'antenne RGP :") # adds text to text bo
+            text_box.insert("end", rgp) 
+         
+    
+    observation_time=np.array(observation_time)
+    base_line=np.array(base_line)
+    
+    poids=np.min(base_line)/base_line
+    
+    
+    final=np.c_[coordinates_stat, base_line, observation_time, poids]
+    mean=np.mean(final, axis=0)
+    
+    text_box.insert("end", "Données journalières calculées avec succees \n")  
+    
+    print('Données journalière calculé')
 
-station_list_sira=['sete', 'mtp2', 'mntp', 'yscn', 'gajn', 'sgil']
-station_list_stein=['pzna', 'agds', 'agde', 'sete', 'narb', 'pard', 'mtp2']
-site='SIRA'
-path_download=py_path+"\DOWNLOAD\Donwload_file_RGP"
 
-file_path=path_download+'/'+site+'/'+str(year)+'/'+str(gnss_day)+'/obs'
-file_path_nav=path_download+'/'+site+'/'+str(year)+'/'+str(gnss_day)+'/nav'
-       
-download_rinex_obs(year, gnss_day, station_list_sira, site, path_download)
-uncrompress_Z_file(file_path)
-uncompact_rinex_file_hatanaka(file_path)
-delete_useless_file(file_path)
+    #%%Calcul de la moyenne pondéré
+    x_mean=final[:,[0]].reshape(5)
+    y_mean=final[:,[1]].reshape(5)
+    z_mean=final[:,[2]].reshape(5)
+    
+    x_poids=x_mean*poids
+    y_poids=y_mean*poids
+    z_poids=z_mean*poids
+    
+    x_final=np.sum(x_poids)/np.sum(poids)
+    y_final=np.sum(y_poids)/np.sum(poids)
+    z_final=np.sum(z_poids)/np.sum(poids)
 
-uncrompress_Z_file_nav(file_path_nav)
-Donwload_SIRA_day(year, month, day, station_list_sira, site, path_download, gnss_day)
-unzip_SIRA_file(year, gnss_day)
+#%%SAVE RESULT
+    path_download=py_path+"\\PROCESS_RESULT\\TOTAL_RESULT.txt"
+    
+    with open(path_download, "a") as myfile:
+        myfile.write(str(gnss_day)+'\t'+str(x_final) +'\t' +str(y_final)+'\t'+ str(z_final))
+        myfile.write('\n')
+    
+#%%plot result
+def plot_ecart():
+    
+    py_path=os.getcwd()
+    path_download=py_path+"\\PROCESS_RESULT\\TOTAL_RESULT.txt"
+    
+    lines = np.loadtxt(path_download, comments="#", delimiter="\t", unpack=False)
+    base_coordinates=np.array([4614730.1723, 315963.4502, 4376880.0330]).T
+    
+    lines=lines[lines[:, 0].argsort()]
+    
+    date=lines[:,0]
+    xpoints = lines[:, 1]
+    ypoints = lines[:, 2]
+    zpoints = lines[:, 3]
+    
+    ecart_x=xpoints-base_coordinates[0]
+    ecart_y=ypoints-base_coordinates[1]
+    ecart_z=zpoints-base_coordinates[2]
+    
+    fig, ax = plt.subplots()
+    ax.plot(date, ecart_x, color='b', label='x')
+    ax.plot(date, ecart_y, color='r', label='y')
+    ax.plot(date, ecart_z, color='g', label='z')
+    
+    # ax.xaxis.set_major_formatter(dates.DateFormatter('%y%m%d'))
+    ax.yaxis.set_major_formatter(plt.FormatStrFormatter('%f'))
+    ax.legend(['x', 'y', 'z'])
+    plt.xlabel("GNSS_Day")
+    plt.ylabel("Ecarts")
+    
+    plt.show()
+
+
+
+mainwindow=tk.Tk()
+mainwindow.title("Calculator")
+mainwindow.geometry('800x200')
+
+year_leb = tk.Label(mainwindow, text="Calcul de GNSS : ", bg="lightgrey")
+year_leb.grid(row=0, column=1)
+
+#Ligne 1
+year_leb = tk.Label(mainwindow, text="Année : ", bg="lightgrey")
+year_leb.grid(row=0, column=2)
+
+month_lab = tk.Label(mainwindow, text="Mois : ", bg="lightgrey")
+month_lab.grid(row=0, column=3)
+
+day_lab = tk.Label(mainwindow, text="Jour : ", bg="lightgrey")
+day_lab.grid(row=0, column=4)
+
+#Ligne 2
+enter_info = tk.Label(mainwindow, text="Entrer la date de calcul : ", bg="lightgrey")
+enter_info.grid(row=1, column=1)
+
+year_info = tk.Entry(mainwindow, bd=3)
+year_info.grid(row=1, column=2, padx=5, pady=5)
+
+mont_info = tk.Entry(mainwindow, bd=3)
+mont_info.grid(row=1, column=3, padx=5, pady=5)
+
+day_info = tk.Entry(mainwindow, bd=3)
+day_info.grid(row=1, column=4, padx=5, pady=5)
+
+def getentry_download():
+    year_var = int(year_info.get())
+    month_var =int(mont_info.get())
+    day_var = int(day_info.get())
+    
+    dobnwload_gnss(year_var, month_var, day_var)
+    
+def process_gnss_tk():
+    year_var = int(year_info.get())
+    month_var =int(mont_info.get())
+    day_var = int(day_info.get())
+    
+    process_gnss(year_var, month_var, day_var)
+    
+def stat_global(): 
+    py_path=os.getcwd()
+    path_download=py_path+"\\PROCESS_RESULT\\TOTAL_RESULT.txt"
+    
+    lines = np.loadtxt(path_download, comments="#", delimiter="\t", unpack=False)
+    
+    xyz=lines[:,[1,2,3]]
+    mean=np.mean(xyz, axis=0)
+    std=np.std(xyz, axis=0)
+    
+    print(mean)
+    print(std)
+    
+    text_box.delete('1.0', tk.END)
+    
+    text_box.insert("end", mean) # adds text to text bo
+    text_box.insert("end", "\n")
+    text_box.insert("end", std)
+    
+    return mean, std
+
+def get_dernier_jour(): 
+    py_path=os.getcwd()
+    path_download=py_path+"\\PROCESS_RESULT\\TOTAL_RESULT.txt"
+    
+    lines = np.loadtxt(path_download, comments="#", delimiter="\t", unpack=False)
+    
+    day=lines[:,0]
+    max_day=int(np.max(day))
+    
+    day_year=gnsscal.yrdoy2date(2024, max_day)
+    
+    text_box.delete('1.0', tk.END)
+    text_box.insert("end", day_year) # adds text to text bo
+    
+
+get_day = tk.Button(mainwindow, text ="Get Last Day", command = get_dernier_jour)
+get_day.grid(row=1, column=5, padx=5, pady=5)
+    
+
+#Button
+
+T_button = tk.Button(mainwindow, text ="Telechargement Data", command = getentry_download)
+T_button.grid(row=2, column=2, padx=5, pady=5)
+
+C_button = tk.Button(mainwindow, text ="Process GNSS day", command = process_gnss_tk)
+C_button.grid(row=2, column=3, padx=5, pady=5)
+
+Plot_button = tk.Button(mainwindow, text ="Plot result", command = plot_ecart)
+Plot_button.grid(row=2, column=4, padx=5, pady=5)
+
+Plot_button = tk.Button(mainwindow, text ="Get Stat", command = stat_global)
+Plot_button.grid(row=3, column=3, padx=5, pady=5)
+
+text_box = tk.Text(mainwindow, width = 70, height = 3)
+text_box.grid(row = 4, column = 2, columnspan=3)
+
+
+tk.mainloop()
+
+    
--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import os 
 from tqdm import tqdm
 import zipfile
+import gzip
 
 def download_rinex_obs(year, gnss_day, station_list, site, path_download):
     
@@ -72,7 +73,6 @@ def uncrompress_Z_file_nav(file_path_nav):
             new_file=file.replace('24n.Z', 'n')
         if '24g.Z' in file: 
             new_file=file.replace('24g.Z', 'g')
-        print(file)
         uncompressed_data = unlzw3.unlzw(Path(file_path_nav+'/'+file))
         with open(file_path_nav+'/'+new_file, 'wb') as f:
             f.write(uncompressed_data)
@@ -88,6 +88,9 @@ def uncompact_rinex_file_hatanaka(file_path):
         if 'crx' in file: 
             crx_file=file_path+'/'+file
             os.system(hatanaka_path+' '+crx_file)
+            rename_o=crx_file.replace('crx', 'o')
+            rename_rnx=crx_file.replace('crx', 'rnx')
+            os.rename(rename_rnx, rename_o)
 
 def delete_useless_file(file_path): 
     list_file=os.listdir(file_path)
@@ -102,6 +105,11 @@ def Donwload_SIRA_day(year, month, day, station_list, site, path_download, gnss_
         month=str(month)
     else:
         month='0'+str(month)
+        
+    if day>10: 
+        day=str(day)
+    else:
+        day='0'+str(day)
     path = '/FTP/SIRA/0712/'+str(year)+'/'+month+'/'+str(day)
     ftp = ftplib.FTP("192.9.200.231") 
     ftp.login('GPS', ']!HlKi') 
@@ -136,9 +144,77 @@ def Donwload_IGS_product(gnss_week, gnss_day, year):
     ftp.cwd(path)
     # donwload_path = path_download+'/'+site
     
+    donwload_path = os.getcwd()+'/DOWNLOAD/Donwload_file_RGP/SIRA'+"/"+str(year)+'/'+str(gnss_day)+'/'+'IGS'
+    
+    try:
+        os.mkdir(donwload_path)
+    except: 
+        pass
+    
     filename_sp3='IGS0OPSRAP_'+str(year)+str(gnss_day)+'0000_01D_15M_ORB.SP3.gz'
     filename_clk='IGS0OPSRAP_'+str(year)+str(gnss_day)+'0000_01D_05M_CLK.CLK.gz'
     
-    ftp.retrbinary("RETR " + filename, open(donwload_station_path,'wb').write)
+    donwload_sp3_path=donwload_path+'/'+filename_sp3
+    ftp.retrbinary("RETR " + filename_sp3, open(donwload_sp3_path,'wb').write)
+    
+    donwload_clk_path=donwload_path+'/'+filename_clk
+    ftp.retrbinary("RETR " + filename_clk, open(donwload_clk_path,'wb').write)
+    
+    
+    with gzip.open(donwload_clk_path, 'rb') as fclk:
+      file_content_1 = fclk.read()
 
+    donwload_clk_path_un=donwload_clk_path.replace('.CLK.gz', 'SP3')
+    with open(donwload_clk_path_un+'.clk', "wb") as binary_file_1:
+      # Write bytes to file
+      binary_file_1.write(file_content_1)
+      
+      '''-----'''
+      
+    with gzip.open(donwload_sp3_path, 'rb') as f:
+      file_content = f.read()
+    
+    donwload_sp3_path_un=donwload_sp3_path.replace('.SP3.gz', 'SP3')
+    with open(donwload_sp3_path_un+'.sp3', "wb") as binary_file:
+      # Write bytes to file
+      binary_file.write(file_content)
+      
+    os.remove(donwload_clk_path)
+    os.remove(donwload_sp3_path)
+
+    
 #%%Calul GNSS
+def calcul_GNSS_RTK_LIB(site, site_num ,year, month, day, gnss_day, gnss_week, rgp_obs_file):
+    root_path=os.getcwd()
+    folder_process=str(year)+str(month)+str(day)
+    process_path=root_path+'/PROCESS_RESULT/'+folder_process
+    try: 
+        os.mkdir(process_path)
+    except: 
+        pass
+    
+    py_path=os.getcwd()
+    RTK_LIB=py_path+'\\RTKLIB\\bin\\rnx2rtkp.exe '
+    config_file=py_path+'\\RTKLIB\\config_file.conf '
+    
+    obs_nav_file_folder=py_path+'\\DOWNLOAD\\Donwload_file_RGP\\'+site+'\\'+str(year)+'\\'+str(gnss_day)+'\\Antenne\\'
+    list_obs=os.listdir(obs_nav_file_folder)
+    for item in list_obs: 
+        if 'o' in item: 
+            obs_file=obs_nav_file_folder+item+' '
+        if 'p' in item: 
+            nav_file=obs_nav_file_folder+item+' '
+    
+    rgp_obs=py_path+'\\DOWNLOAD\\Donwload_file_RGP\\'+site+'\\'+str(year)+'\\'+str(gnss_day)+'\\obs\\'+rgp_obs_file+str(gnss_day)+'0.o '
+    
+    sp3_file=py_path+'\\DOWNLOAD\\Donwload_file_RGP\\'+site+'\\'+str(year)+'\\'+str(gnss_day)+'\\IGS\\'+'IGS0OPSRAP_'+str(year)+str(gnss_day)+'0000_01D_15M_ORBSP3.sp3 '
+    clk_file=py_path+'\\DOWNLOAD\\Donwload_file_RGP\\'+site+'\\'+str(year)+'\\'+str(gnss_day)+'\\IGS\\'+'IGS0OPSRAP_'+str(year)+str(gnss_day)+'0000_01D_05M_CLKSP3.clk '
+    
+    out_file=process_path+'\\'+rgp_obs_file+'_result.out'
+    
+    command=RTK_LIB+obs_file+rgp_obs+nav_file+sp3_file+clk_file+'-k '+config_file+' -o '+out_file
+        
+    os.system(command)
+    
+    return out_file
+    
